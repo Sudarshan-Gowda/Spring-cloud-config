618ae0de6cca26c250ad5d268ebbfe655ea256e2
Sudarshan <sudarshan@Sudarshans-MacBook-Pro.local>
2020-08-31 23:07:19 +0800

    Base Release

---
+++ spring-config-sever/src/main/java/com/star/sud/SpringBootConfigServer.java
@@ -0,0 +1,15 @@
+package com.star.sud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringBootConfigServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigServer.class, args);
+	}
+
+}
